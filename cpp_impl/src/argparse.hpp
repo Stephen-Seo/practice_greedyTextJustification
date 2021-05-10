aa49e41a80d322575553598fb6f299f8e53b8e4a
Stephen Seo <seo.disparate@gmail.com>
2021-05-10 14:32:47 +0900

    Impl arg parsing

--- cpp_impl/src/argparse.hpp
+++ cpp_impl/src/argparse.hpp
@@ -3,9 +3,14 @@
 
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 namespace ArgParse {
-    std::unordered_map<std::string, std::string> parseArgs(int argc, char **argv);
+    std::unordered_map<std::string, std::string> parseArgs(
+        int argc,
+        char **argv,
+        const std::vector<std::string> &simpleArgs,
+        const std::vector<std::string> &pairedArgs);
 } // namespace ArgParse
 
 #endif
--- cpp_impl/src/main.cpp
+++ cpp_impl/src/main.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
 
+#include "argparse.hpp"
 
 int main(int argc, char **argv) {
+    auto parsed = ArgParse::parseArgs(argc, argv, {"a", "b"}, {"apple"});
+
+    for(auto pair : parsed) {
+        std::cout << pair.first << ", " << pair.second << std::endl;
+    }
+
     return 0;
 }
